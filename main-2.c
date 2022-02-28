d32e3eac479633dee66e62d9be2cec3dc0c3970c
cyrox007 <cyrox007@gmail.com>
2022-02-28 16:42:02 +0300

    x

--- main-2.c
+++ main-2.c
@@ -4,14 +4,14 @@ int main(int argc, char const *argv[])
 {
     int x; // Здесь будем хранить килограммы
     int cost = 5;
-    float sale = 20;
+    float sale = 20.0f;
     
     printf("Enter the weight: \n");
     scanf("%d", &x);
 
-    int res = x * cost - ((x * cost) * (sale / 100));
+    float res = x * cost - ((x * cost) * (sale / 100));
 
-    printf("Result: %d Euro \n", res);
+    printf("Result: %.f Euro \n", res);
 
     return 0;
 }
--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main(int argc, char const *argv[])
     float cost = 0.20f;
     int electr = 150;
 
-    int res = cost * electr;
-    printf("Result: $%d", res);
+    float res = cost * (float)electr;
+    printf("Result: $%2.f", res);
 
     return 0;
 }
